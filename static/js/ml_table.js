a066748700f5f8e9169161dd0a91f397efc87911
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-05-26 20:46:00 +0800

    filters but doesn't unfilter

--- static/js/ml_table.js
+++ static/js/ml_table.js
@@ -6,7 +6,7 @@
 // });
 
 // Append search bar and placeholder in the search
-d3.select("#ml_table")
+d3.select(".searching")
     .attr("class", "SearchBar")
     .append("p")
     .attr("class", "SearchBar")
@@ -29,16 +29,16 @@ d3.select(".SearchBar")
 /////////////////////////////////
 
 // Append table heading
-var column_names = ["Year","Country","Actual Happiness Score","Predicted Happiness Score"];
+// var column_names = ["Year","Country","Actual Happiness Score","Predicted Happiness Score"];
 
-var table = d3.select("#ml_table").append("table").attr("class", "table-bordered tab");
-table.append("thead").append("tr"); 
+// var table = d3.select("#ml_table").append("table").attr("class", "table-bordered tab");
+// table.append("thead").append("tr"); 
 
-var headers = table.select("tr").selectAll("th")
-    .data(column_names)
-    .enter()
-    .append("th")
-    .text((d) => d); //in entries in column_names array
+// var headers = table.select("tr").selectAll("th")
+//     .data(column_names)
+//     .enter()
+//     .append("th")
+//     .text((d) => d); //in entries in column_names array
 
 /////////////////////////////////
 //Append table data entries
@@ -46,16 +46,24 @@ var rows, row_entries, row_entries_no_anchor, row_entries_with_anchor;
 
 d3.json("/ml_json").then((data) => { // loading data from server
   
+    const tbody = d3.select("tbody");
+
+    // Year 
     yr = data.map(elem =>elem.year);
     console.log(yr);
-
+    console.log(typeof(yr));
+    // Country
     country = data.map(elem => elem.country);
+    console.log(typeof(country));
+    // y_actual (Happiness Score)
     y_actual = data.map(elem => elem.y_actual);
+    console.log(typeof(y_actual));
+    // y_predicted (Happiness Score)
     y_predicted = data.map(elem => elem.y_predicted);
 
-    // draw table body with rows
+    // Draw table body with rows
     for (var i = 0; i < 70; i++) {
-        trow = table.append("tr")
+        trow = tbody.append("tr")
         trow.append("td").text(yr[i])
         trow.append("td").text(country[i])
         trow.append("td").text(y_actual[i])
@@ -64,19 +72,57 @@ d3.json("/ml_json").then((data) => { // loading data from server
 
     //////////////////////////////////////////
 
-    d3.select("#search")
-    .on("keyup", function() { // filter according to key pressed 
-      var searched_data = data,
-          text = this.value.trim();
+    d3.select("#search").on("keyup", function() { // filter according to key pressed 
+        var searched_data = data,
+        text = this.value.trim();
       
-          var searchResults = searched_data.map(function(r) {
+        var searchResults = searched_data.map(function(r) {
+
+            // r.yr = parseInt(r.yr); 
+
             var regex = new RegExp("^" + text + ".*", "i");
             if (regex.test(r.country)) { // if there are any results
-              console.log(r.country);
-              console.log(r.y_actual);
+                console.log(r.year);
+                console.log(r.country);
+                console.log(r.yr);
+                console.log("y_actual: " + r.y_actual);
+                console.log("y_predicted: " + r.y_predicted);
+
+                // Clear out what was in body before
+                // table = d3.select("#ml_table")
+                tbody.html("");
+
+                // for (var i = 0; i<20; i++) {
+                trow = tbody.append("tr")
+                trow.append("td").text(r.year);
+                trow.append("td").text(r.country);
+                trow.append("td").text(r.y_actual);
+                trow.append("td").text(r.y_predicted);
+                // };
+
+            // for (var i = 0; i < 10; i++) {
+            //     trow = table.append("tr");
+            //     trow.append("td").text(r.yr[i]);
+            //     trow.append("td").text(r.country[i]);// test row
+            //     trow.append("td").text(r.y_actual[i]); 
+            //     trow.append("td").text(r.y_actual[i]);
+            // };
+            };
+
+        });
+
+
+    });
+
+
+
+
+
                 // return regex.exec(r.country)[0]; // return them to searchResults
-            } 
-          })
+        //     }
+            
+            
+        //   })
           
         //   // filter blank entries from searchResults
         //   searchResults = searchResults.filter(function(r){ 
@@ -164,7 +210,7 @@ d3.json("/ml_json").then((data) => { // loading data from server
         // if (regex.test(r.country)) { // if there are any results
         //   return regex.exec(r.country)[0]; // return them to searchResults
         // } 
-      })
+    //   })
 
 
 
