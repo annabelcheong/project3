e66b2e78a1718203328a6cef0009adededc4323b
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-05-26 18:04:20 +0800

    printed the happiness score table

--- static/js/ml_table.js
+++ static/js/ml_table.js
@@ -1,9 +1,9 @@
 // insert into <div id='ml_table'> TABLE GOES HERE </div>
 
 // Use API endpoint: ml_json (See file app.py)
-d3.json('/ml_json').then((data) => {
-    console.log(data);
-});
+// d3.json('/ml_json').then((data) => {
+//     // console.log(data);
+// });
 
 // Append search bar and placeholder in the search
 d3.select("#ml_table")
@@ -17,28 +17,78 @@ d3.select(".SearchBar")
     .attr("class", "SearchBar")
     .attr("id", "search")
     .attr("type", "text")
-    .attr("placeholder", "Spain");
+    .attr("placeholder", "Spain")
+    .enter()
+    .append("br");
 
+    // d3.select(".SearchBar")
+    // .append("br")
+    // .append("hr");
+
+
+
+ 
 // Append table heading
-var column_names = ["year","country","y_actual","y_predicted"];
-// CREATE TABLE happy_table_m2 (
-//     id INT PRIMARY KEY,  
-//     year INT NOT NULL,
-//     country VARCHAR NOT NULL,
-//     logged_gdp_per_capita DEC NOT NULL,
-//     support DEC NOT NULL,
-//     life_exp DEC NOT NULL,
-//     freedom DEC NOT NULL,
-//     generosity DEC NOT NULL,
-//     corruption DEC NOT NULL,
-//     y_actual DEC NOT NULL,
-//     y_predicted DEC NOT NULL
-//   );
-var table = d3.select("#ml_table").append("table");
+var column_names = ["Year","Country","Actual Happiness Score","Predicted Happiness Score"];
+
+var table = d3.select("#ml_table").append("table").attr("class", "table-bordered");
 table.append("thead").append("tr"); 
 
 var headers = table.select("tr").selectAll("th")
     .data(column_names)
     .enter()
     .append("th")
-    .text(function(d) { return d; });
+    .text((d) => d); //in entries in column_names array
+
+var rows, row_entries, row_entries_no_anchor, row_entries_with_anchor;
+
+
+d3.json("/ml_json").then((data) => { // loading data from server
+  
+    yr = data.map(elem =>elem.year);
+    console.log(yr);
+
+    country = data.map(elem => elem.country);
+    y_actual = data.map(elem => elem.y_actual);
+    y_predicted = data.map(elem => elem.y_predicted);
+
+    // draw table body with rows
+    for (var i = 0; i < 70; i++) {
+        trow = table.append("tr")
+        trow.append("td").text(yr[i])
+        trow.append("td").text(country[i])
+        trow.append("td").text(y_actual[i])
+        trow.append("td").text(y_predicted[i])
+        // .text(country[i])
+    };
+
+
+    // trow = tbody.append("tr");
+    // trow.append("td").text(data.year[i]);
+        // .text(data.year);
+
+//   // data bind
+//   rows = table.select("tbody").selectAll("tr")
+//     .data(data, function(data){ return data.year; });
+  
+//   // enter the rows
+//   rows.enter()
+//     .append("tr")
+  
+//   // enter td's in each row
+//   row_entries = rows.selectAll("td")
+//       .data(function(data) { 
+//         var arr = [];
+//         for (var k in data {
+//           if (data.hasOwnProperty(k)) {
+// 		    arr.push(data[k]);
+//           }
+//         }
+//         return [arr[0],arr[1],arr[2],arr[3]];
+//       })
+//     .enter()
+//       .append("td") 
+
+//       console.log(data);
+
+});
