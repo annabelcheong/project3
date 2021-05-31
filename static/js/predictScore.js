566d39936aa54f685a1d06ffeecac29c3fe2e176
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-05-31 21:18:40 +0800

    cleaned predicscore js

--- static/js/predictScore.js
+++ static/js/predictScore.js
@@ -1,3 +1,4 @@
+// EVENT LISTENER when user clicks on 'Predict Happiness Score'
 d3.select("#predict_but").on("click", function(year) {
     
     // Clear previous user output happiness score 
@@ -8,27 +9,27 @@ d3.select("#predict_but").on("click", function(year) {
 
     year = d3.select("#year").property("value")
     // year =2020 // TEST
-    console.log(year)
+    console.log(year) // TEST
 
     // gdp = 50000 // TEST
     gdp = d3.select("#gdp").property("value")
-    console.log(gdp)
+    // console.log(gdp) // TEST
 
     life_exp = d3.select("#life_exp").property("value")
-    console.log(life_exp)
-    // life_exp=70
+    // console.log(life_exp) // TEST
+    // life_exp=70 // TEST
 
     support = d3.select("#support").property("value")
-    // console.log(support)
+    // console.log(support) // TEST
 
     freedom = d3.select("#freedom").property("value")
-    // freedom=0.44
+    // freedom=0.44 // TEST
 
     generosity = d3.select("#generosity").property("value")
-    // generosity = 0.2
+    // generosity = 0.2 // TEST
 
     corruption = d3.select("#corruption").property("value")
-    // corruption =0.4
+    // corruption =0.4 // TEST
 
     // Read in flask end point
     d3.json(`/predict/${year}/${gdp}/${life_exp}/${support}/${freedom}/${generosity}/${corruption}`).then((data) => {
@@ -40,49 +41,9 @@ d3.select("#predict_but").on("click", function(year) {
         .append("h3")
         .attr("class", "text-muted")
         .text(data);
-        // .append("br");
-
-
-
-
-    // <h3> <span class="text-muted"
-
-
-    // append("td").text(event_addy[i]);
-    // Use to enter year:
-    // d3.select(".predict_class")
-    // .append("p")
-    // .attr("class", "searchPredict")
-    // .text("Enter values for year:")
-    // .append("br");
-
-    // // Append year box:
-    // d3.select(".SearchPredict")
-    // .append("input")
-    // .attr("class","searchPredict")
-    // .attr("type","number")
-    // .attr("placeholder","2021")
-    // .enter();
-
-    // // Use to enter GDP/capita value:
-    // d3.select(".predict_class")
-    // .append("p")
-    // .attr("class", "searchPredict")
-    // .text("Enter values for gdp/capita:")
-    // .append("br");
-
-    // // Append GDP/capita box:
-    // d3.select(".SearchPredict")
-    // .append("input")
-    // .attr("class","searchPredict")
-    // .attr("type","number")
-    // .attr("placeholder","GDP/capita (USD)")
-    // .enter();
-
 
     });
-
-    
+  
 });
 
 
